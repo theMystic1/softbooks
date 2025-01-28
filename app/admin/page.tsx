bc08b3c493905fbd37e2871800c6ebfcb7f29556
themystic1 <cluckyugo@gmail.com>
2025-01-28 07:06:13 +0100

    ..

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,5 @@
 function page() {
-  return <div>admin</div>;
+  return <div>admin page</div>;
 }
 
 export default page;
--- database/drizzle.ts
+++ database/drizzle.ts
@@ -1,6 +1,5 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-// import { config } from "dotenv";
 import { Config } from "@/tsconfig/envconfig";
 
 const sql = neon(Config.databaseurl);
